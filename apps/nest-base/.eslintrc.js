72ed0320103bcac83aeb126a9bd79cb2a179ca74
Titux Metal (DEV) <github@lgdweb.fr>
2024-09-07 09:51:29 +0200

    chore: Update eslint configuration for Nest Base app

--- apps/nest-base/.eslintrc.js
+++ apps/nest-base/.eslintrc.js
@@ -1,25 +1,22 @@
+const { resolve } = require('node:path')
+
+const project = resolve(__dirname, 'tsconfig.json')
+
+/** @type {import("eslint").Linter.Config} */
 module.exports = {
+  root: true,
+  extends: ['@repo/eslint-config/nest.js'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
+    project,
     tsconfigRootDir: __dirname,
-    sourceType: 'module',
+    sourceType: 'module'
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['node_modules/', 'dist/', '.eslintrc.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
-};
+    '@typescript-eslint/no-unsafe-return': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off', // This rule is too strict for the way we use jest
+    '@typescript-eslint/no-unsafe-member-access': 'off', // This rule is too strict for the way we use jest
+    'jest/expect-expect': 'off'
+  }
+}
