fb630d89965ab0cc6c19a8c643a3ff1fe0a0d81a
Titux Metal (DEV) <github@lgdweb.fr>
2024-09-09 15:54:11 +0200

    chore: Update vite.config.ts configuration in Remix base app

--- apps/remix-base/vite.config.ts
+++ apps/remix-base/vite.config.ts
@@ -1,20 +1,44 @@
+import { resolve } from 'node:path'
+
 import { vitePlugin as remix } from '@remix-run/dev'
+import { flatRoutes } from 'remix-flat-routes'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
+const MODE = process.env.NODE_ENV
+
+// installGlobals()
+
 export default defineConfig({
-  server: { port: 3000 },
+  server: {
+    port: 3000
+  },
+  resolve: {
+    preserveSymlinks: true
+  },
+  build: {
+    cssMinify: MODE === 'production',
+    sourcemap: false,
+    commonjsOptions: {
+      include: [/remix-base/, /nest-base/, /node_modules/]
+    }
+  },
   plugins: [
     remix({
+      ignoredRouteFiles: ['**/*.css'],
+      serverModuleFormat: 'esm',
       future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true
+        // v3_fetcherPersist: true,
+        // v3_relativeSplatPath: true,
+        // v3_throwAbortReason: true
       },
-      appDirectory: 'app',
-      ignoredRouteFiles: ['**/.*.css'],
-      serverModuleFormat: 'esm'
+      routes: defineRoutes => {
+        return flatRoutes('routes', defineRoutes, {
+          ignoredRouteFiles: ['.*', '**/*.css', '**/*.test.{js,jsx,ts,tsx}', '**/__*.*'],
+          appDir: resolve(__dirname, 'app')
+        })
+      }
     }),
-    tsconfigPaths()
+    tsconfigPaths({})
   ]
 })
