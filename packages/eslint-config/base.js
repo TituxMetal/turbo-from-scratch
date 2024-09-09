c30d4bc0ea1efe49e1ca469471674e48a7c9dd83
Titux Metal (DEV) <github@lgdweb.fr>
2024-09-09 16:18:32 +0200

    core: Update the eslint-config package

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,45 +1,21 @@
-const { resolve } = require('node:path')
-
-const project = resolve(process.cwd(), 'tsconfig.json')
-
+/** @type {import('@types/eslint').Linter.Config} */
 module.exports = {
   extends: [
-    '@vercel/style-guide/eslint/browser',
+    '@vercel/style-guide/eslint/node',
     '@vercel/style-guide/eslint/typescript',
-    '@vercel/style-guide/eslint/react',
-    '@vercel/style-guide/eslint/jest',
-    '@vercel/style-guide/eslint/jest-react'
+    'eslint-config-turbo'
   ].map(require.resolve),
+  parser: '@typescript-eslint/parser',
   env: {
-    browser: true,
-    commonjs: true,
-    es6: true,
     node: true
   },
-  globals: {
-    React: true,
-    JSX: true
-  },
-  settings: {
-    'import/resolver': {
-      typescript: {
-        project
-      }
-    }
-  },
-  ignorePatterns: ['node_modules/', 'build/', '.eslintrc.cjs', '**/*.css'],
   rules: {
-    semi: ['error', 'never'],
     quotes: ['error', 'single'],
-    'no-console': 'off',
-    '@typescript-eslint/no-shadow': 'off',
-    'import/no-default-export': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-unsafe-assignment': 'off',
-    'react/function-component-definition': [
-      'error',
-      { namedComponents: 'arrow-function', unnamedComponents: 'arrow-function' }
-    ],
+    semi: ['error', 'never'],
+
+    'import/consistent-type-specifier-style': ['warn', 'prefer-inline'],
+    'import/no-duplicates': ['warn', { 'prefer-inline': true }],
+
     '@typescript-eslint/consistent-type-imports': [
       'warn',
       {
