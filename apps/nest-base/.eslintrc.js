bb7aaf3d4674c6800f18bd9014c8d750441a9fb0
Titux Metal (DEV) <github@lgdweb.fr>
2024-09-09 16:20:18 +0200

    chore: Update eslint configuration in all apps

--- apps/nest-base/.eslintrc.js
+++ apps/nest-base/.eslintrc.js
@@ -5,18 +5,38 @@ const project = resolve(__dirname, 'tsconfig.json')
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ['@repo/eslint-config/nest.js'],
+  extends: ['@repo/eslint-config/base.js'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project,
+    project: true,
     tsconfigRootDir: __dirname,
     sourceType: 'module'
   },
   ignorePatterns: ['node_modules/', 'dist/', '.eslintrc.js'],
+  settings: {
+    'import/resolver': {
+      alias: {
+        extensions: ['.ts'],
+        map: [['~', './src']]
+      },
+      typescript: {
+        project
+      }
+    }
+  },
   rules: {
-    '@typescript-eslint/no-unsafe-return': 'off',
-    '@typescript-eslint/no-unsafe-call': 'off', // This rule is too strict for the way we use jest
-    '@typescript-eslint/no-unsafe-member-access': 'off', // This rule is too strict for the way we use jest
-    'jest/expect-expect': 'off'
+    'no-console': 'off',
+
+    'import/no-cycle': 'off',
+    // 'import/no-extraneous-dependencies': 'off',
+
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    // '@typescript-eslint/explicit-module-boundary-types': 'off',
+    // '@typescript-eslint/interface-name-prefix': 'off',
+    // '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-extraneous-class': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off'
   }
 }
