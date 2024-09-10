c29821cf1fd5a432d2a68bb5b44309abab67f16e
Titux Metal (DEV) <github@lgdweb.fr>
2024-09-10 10:48:17 +0200

    feat: Add RemixController and RemixService to handle Remix app requests in nest app

--- apps/nest-base/src/app.module.ts
+++ apps/nest-base/src/app.module.ts
@@ -3,9 +3,12 @@ import { Module } from '@nestjs/common'
 import { AppController } from '~/app.controller'
 import { AppService } from '~/app.service'
 
+import { RemixController } from './remix/remix.controller'
+import { RemixService } from './remix/remix.service'
+
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService]
+  controllers: [AppController, RemixController],
+  providers: [AppService, RemixService]
 })
 export class AppModule {}
