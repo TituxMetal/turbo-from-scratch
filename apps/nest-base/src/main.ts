7901e7b7cb1e8fb9824c88c3e2858cea9efef414
Titux Metal (DEV) <github@lgdweb.fr>
2024-09-10 10:49:49 +0200

    feat: Add Remix app support to Nest app
    This commit adds support for integrating a Remix app into a Nest app. The main.ts file is updated to start the Remix dev server and configure static assets for the Remix app. The app now listens on the selected port, which can be specified through the environment variable PORT or defaults to 3000.

--- apps/nest-base/src/main.ts
+++ apps/nest-base/src/main.ts
@@ -1,10 +1,28 @@
 import { NestFactory } from '@nestjs/core'
 import { type NestExpressApplication } from '@nestjs/platform-express'
+import { getPublicDir, startDevServer } from '@repo/remix-app'
 
 import { AppModule } from '~/app.module'
 
 const bootstrap = async () => {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule)
-  await app.listen(3001)
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    bodyParser: false
+  })
+
+  await startDevServer(app)
+
+  console.log(getPublicDir())
+
+  app.useStaticAssets(getPublicDir(), {
+    immutable: true,
+    maxAge: '1y',
+    index: false
+  })
+
+  const selectedPort = process.env.PORT ?? 3000
+
+  console.log(`Running on port http://localhost:${selectedPort.toString()}`)
+  await app.listen(selectedPort)
 }
+
 bootstrap()
